7764f459d0f59ee9853f318210e5f1a0af2adbfd
Jorge Luis Olarte Vargas <jolarte8811@gmail.com>
2021-11-09 11:22:11 -0500

    Initial commit

---
+++ app/src/main/java/com/jolarte/codetravel_jardin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jolarte.codetravel_jardin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
